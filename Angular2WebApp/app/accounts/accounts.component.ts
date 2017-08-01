43cfd2a6b875e7a9a4bc4bc0046da2840778f033
khanhlive <khanhngdinh@outlook.com>
2017-08-02 06:54:06 +0700

    updated 02/08/2017
    updated 02/08/2017

---
+++ Angular2WebApp/app/accounts/accounts.component.ts
@@ -0,0 +1,10 @@
+﻿import {Component} from '@angular/core'
+
+@Component({
+    selector: 'my-acc',
+    templateUrl: 'app/accounts/accounts.component.html',
+    styleUrls:['app/accounts/accounts.component.css']
+})
+export class AccountComponent {
+    header: string='My Account'
+}
